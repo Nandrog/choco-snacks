dd8c8659e288ca936717027472a52af8170a5954
Nandrog <naturinda.rodgers@students.mak.ac.ug>
2025-04-14 14:19:51 +0300

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,181 @@
+const products = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/image1.jpg",
+    name: "Chocolate Jelly Sliced Cake",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 390
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/products/image2.jpg",
+    name: "3-Layered Chocolate Cake",
+    rating: {
+      stars: 4,
+      count: 1270
+    },
+    priceCents: 209
+  },
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/image3.jpg",
+    name: "Chocolate Spring Cake",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799
+  },
+  {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: "images/products/image4.jpg",
+    name: "Purely Chocolate",
+    rating: {
+      stars: 5,
+      count: 2197
+    },
+    priceCents: 299
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: "images/products/image5.jpg",
+    name: "Chocolate Cream Donuts",
+    rating: {
+      stars: 4,
+      count: 37
+    },
+    priceCents: 267
+  },
+  {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: "images/products/image6.jpg",
+    name: "Brownies 3-slices",
+    rating: {
+      stars: 4.5,
+      count: 317
+    },
+    priceCents: 398
+  },
+  {
+    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    image: "images/products/image7.jpg",
+    name: "Chocolate Chips Cookies",
+    rating: {
+      stars: 4.5,
+      count: 144
+    },
+    priceCents: 599
+  },
+  {
+    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    image: "images/products/image8.jpg",
+    name: "Chocolate Rainbow Roll",
+    rating: {
+      stars: 4.5,
+      count: 305
+    },
+    priceCents: 299
+  },
+  {
+    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    image: "images/products/image9.jpg",
+    name: "Chocolate Berry Slice",
+    rating: {
+      stars: 4,
+      count: 89
+    },
+    priceCents: 398
+  },
+  {
+    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    image: "images/products/image10.jpg",
+    name: "Chocolate Berry Cake",
+    rating: {
+      stars: 4.5,
+      count: 235
+    },
+    priceCents: 270
+  },
+  {
+    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    image: "images/products/image11.jpg",
+    name: "Choco Deep Bars",
+    rating: {
+      stars: 4.5,
+      count: 30
+    },
+    priceCents: 150
+  },
+  {
+    id: "04701903-bc79-49c6-bc11-1af7e3651358",
+    image: "images/products/image12.jpg",
+    name: "Chocolate Cupcakes -6 pieces",
+    rating: {
+      stars: 4.5,
+      count: 562
+    },
+    priceCents: 299
+  },
+  {
+    id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+    image: "images/products/image13.jpg",
+    name: "Choco Balls-A Plate",
+    rating: {
+      stars: 4,
+      count: 60
+    },
+    priceCents: 369
+  },
+  {
+    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    image: "images/products/image14.jpg",
+    name: "Chocolate Jelly Paste",
+    rating: {
+      stars: 4,
+      count: 99
+    },
+    priceCents: 1740
+  },
+  {
+    id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+    image: "images/products/image15.jpg",
+    name: "Dark Brown Jellied Cake",
+    rating: {
+      stars: 4,
+      count: 215
+    },
+    priceCents: 220
+  },
+  {
+    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    image: "images/products/image16.jpg",
+    name: "Chocolate Waffles",
+    rating: {
+      stars: 4,
+      count: 99
+    },
+    priceCents: 237
+  },
+  {
+    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    image: "images/products/image17.jpg",
+    name: "Berry sliced Cake",
+    rating: {
+      stars: 4,
+      count: 99
+    },
+    priceCents: 398
+  },{
+    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    image: "images/products/image.jpg",
+    name: "Brick Choco Waffles",
+    rating: {
+      stars: 4,
+      count: 99
+    },
+    priceCents: 234
+  }
+];
